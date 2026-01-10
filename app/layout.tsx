e256703e6df79b227722603c175665b7fbfb72e1
the-boring-newspaper <soelinhtoot@gmail.com>
2026-01-09 22:14:25 -0800

    Add GA4 analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,24 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="my">
+<head>
+    <script
+      async
+      src="https://www.googletagmanager.com/gtag/js?id=G-N5C423R0KJ"
+    />
+    <script
+      dangerouslySetInnerHTML={{
+        __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-N5C423R0KJ', {
+            anonymize_ip: true
+          });
+        `,
+      }}
+    />
+  </head>
       <body>
         {children}
       </body>
